e7a57ca0dcd6ec263c835c6a75e324807d2719c6
Spencer H <SpencerHanson3141@gmail.com>
2023-05-10 10:58:40 -0600

    Basic package and repo structure

---
+++ README.rst
@@ -0,0 +1,10 @@
+###########
+Simply-NWB
+###########
+
+- Please see the documentation at **TODO PUT LINK HERE**
+
+- GitHub available with more information `here <https://github.com/Anschutz-Visual-Cluster/simply-nwb>`_
+
+
+Made for CU Anschutz Vision Cluster
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+
+def parse_requirements(requirement_file):
+    with open(requirement_file) as fi:
+        return fi.readlines()
+
+
+with open('./README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='simply_nwb',
+    packages=['simply_nwb', 'simply_nwb.tools', 'simply_nwb.transforms'],
+    version=VERSION,
+    description='Common NWB use cases and simplified interface for common usage',
+    author='Spencer Hanson',
+    long_description=long_description,
+    install_requires=parse_requirements('requirements.txt'),
+    keywords=['neuroscience', 'nwb', 'tools', 'science'],
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ]
+)
+
